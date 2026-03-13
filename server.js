25b2ab6c6defb3a5f670d878a2975629e040543b
toasterbath0758 <toasterbath0758@gmail.com>
2026-03-13 11:36:10 +0100

    404 pagina #5

--- server.js
+++ server.js
@@ -65,3 +65,7 @@ app.listen(app.get('port'), function () {
   // Toon een bericht in de console en geef het poortnummer door
   console.log(`Application started on http://localhost:${app.get('port')}`)
 })
+
+app.use((request, response) => {
+  response.render("404.liquid");
+});
