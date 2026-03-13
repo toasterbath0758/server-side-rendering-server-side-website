ed78ac6d0ddace917ad0b0ba6d62e74f20666d73
toasterbath0758 <toasterbath0758@gmail.com>
2026-03-13 11:47:25 +0100

    mobiele navigatie #9

--- server.js
+++ server.js
@@ -54,6 +54,21 @@ app.get('/nieuws/:slug', async function (request, response) {
    // Geef hier eventueel data aan mee
    response.render('artikeldetailpage.liquid')
 })
+
+app.get('/veldverkenner', async function (request, response) {
+   // Render index.liquid uit de Views map
+   // Geef hier eventueel data aan mee
+   response.render('veldverkenner.liquid')
+})
+
+app.get('/collectie', async function (request, response) {
+   // Render index.liquid uit de Views map
+   // Geef hier eventueel data aan mee
+   response.render('collectie.liquid')
+})
+
+
+
 // Maak een POST route voor de index; hiermee kun je bijvoorbeeld formulieren afvangen
 // Hier doen we nu nog niets mee, maar je kunt er mee spelen als je wilt
 app.post('/', async function (request, response) {
