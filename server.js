f498da04f2fe32929500c67e211df63f038d6c63
toasterbath0758 <toasterbath0758@gmail.com>
2026-03-13 11:35:49 +0100

    detailpagina #7

--- server.js
+++ server.js
@@ -43,6 +43,11 @@ app.get('/', async function (request, response) {
    response.render('index.liquid')
 })
 
+app.get('/nieuws/:slug', async function (request, response) {
+   // Render index.liquid uit de Views map
+   // Geef hier eventueel data aan mee
+   response.render('artikeldetailpage.liquid')
+})
 // Maak een POST route voor de index; hiermee kun je bijvoorbeeld formulieren afvangen
 // Hier doen we nu nog niets mee, maar je kunt er mee spelen als je wilt
 app.post('/', async function (request, response) {
